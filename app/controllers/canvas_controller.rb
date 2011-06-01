99e0eda94dcaef735a65636c7462061b13812a4b
Ian McDowell <igmcdowell@gmail.com>
2011-05-31 18:29:14 -0700

    fixed routing bug

--- app/controllers/canvas_controller.rb
+++ app/controllers/canvas_controller.rb
@@ -2,7 +2,7 @@ class CanvasController < ApplicationController
   require 'net/http'
   require 'net/https'
   # GET /auth
-  def showcanvas
+  def signup
    @url = %Q|https://www.facebook.com/dialog/oauth?client_id=#{ENV['FB_ID']}&redirect_uri=#{root_url+'/auth'}&scope=publish_stream,offline_access|
    render :signup
   end
