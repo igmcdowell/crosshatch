37295491b915cf92631bdccb62cb2f60f60f9514
Ian McDowell <igmcdowell@gmail.com>
2011-06-02 13:54:51 -0700

    core service code complete, still need something to handle cron

--- app/controllers/hatcher_controller.rb
+++ app/controllers/hatcher_controller.rb
@@ -1,38 +1,40 @@
 class HatcherController < ApplicationController
   def hatch
-    user = User.find_by_tw_id('182253036') #hardcoding for initial testing.
-    Twitter.configure do |config|
-      config.consumer_key = ENV['TW_KEY']
-      config.consumer_secret = ENV['TW_SECRET']
-      config.oauth_token = user['tw_token']
-      config.oauth_token_secret = user['tw_secret']
-    end
-    client = Twitter::Client.new
-    timeline = client.user_timeline({:since_id => user['last_post'], :include_entities => true})
-    if timeline[0]
-      user['last_post'] = timeline[0]['id']
-      user.save
-    end
-    timeline.each do |tweet|
-      if !tweet['to_user_id'] 
-        text = tweet['text'] 
-        entities = tweet['entities']
-        hashtags = entities['hashtags']
-        mentions = entities['user_mentions']
-        mentions.each do |mention| #I include the leading @ to guard against really bad double encoding
-          text.gsub!('@'+mention['screen_name'],'@'+mention['screen_name']+' (http://t3l.us/'+mention['screen_name']+')')
-        end
-        hashtags.each do |tag|
-          text.gsub!('#'+tag['text'],'#'+tag['text']+' (http://t3l.us/h/'+tag['text']+')')
-        end
-        puts text
+    users = User.all
+    users.each do |user|
+      Twitter.configure do |config|
+        config.consumer_key = ENV['TW_KEY']
+        config.consumer_secret = ENV['TW_SECRET']
+        config.oauth_token = user['tw_token']
+        config.oauth_token_secret = user['tw_secret']
+      end
+      client = Twitter::Client.new
+      timeline = client.user_timeline({:since_id => user['last_post'], :include_entities => true})
+      fbtoken = user['fb_token']
+      if timeline[0]
+        user['last_post'] = timeline[0]['id']
+        user.save
+      end
+      timeline.each do |tweet|
+        if !tweet['to_user_id'] 
+          text = tweet['text'] 
+          entities = tweet['entities']
+          hashtags = entities['hashtags']
+          mentions = entities['user_mentions']
+          mentions.each do |mention| #I include the leading @ to guard against really bad double encoding
+            text.gsub!('@'+mention['screen_name'],'@'+mention['screen_name']+' (http://t3l.us/'+mention['screen_name']+')')
+          end
+          hashtags.each do |tag|
+            text.gsub!('#'+tag['text'],'#'+tag['text']+' (http://t3l.us/h/'+tag['text']+')')
+          end
         
-        
-        path = "https://graph.facebook.com/me/feed"
-        uri = URI.parse(path+'?access_token='+CGI.escape(token))
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-        @response = http.request_post(uri.path+'?'+uri.query, 'message='+text)
+          path = "https://graph.facebook.com/me/feed"
+          uri = URI.parse(path+'?access_token='+CGI.escape(fbtoken))
+          http = Net::HTTP.new(uri.host, uri.port)
+          http.use_ssl = true
+          @response = http.request_post(uri.path+'?'+uri.query, 'message='+text)
+          @body = response.body
+        end
       end
     end
     render :index
