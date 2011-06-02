cdb918fc26ab79c044c6bac345686d3b3fc21584
Ian McDowell <igmcdowell@gmail.com>
2011-06-02 11:30:47 -0700

    changed migration to specify limit => 8 for integers, to address postgresql out of range error

--- db/migrate/20110601235229_add_needed_fields_to_users.rb
+++ db/migrate/20110601235229_add_needed_fields_to_users.rb
@@ -2,14 +2,14 @@ class AddNeededFieldsToUsers < ActiveRecord::Migration
   def self.up  
     create_table :users do |t|
       t.boolean :tw_linked
-      t.integer :tw_id, :unique => true
+      t.integer :tw_id, :unique => true, :limit => 8
       t.string :tw_handle
       t.string :tw_secret
       t.string :tw_token
       t.string :tw_img
       t.string :fb_token
       t.boolean :fb_linked
-      t.integer :last_post
+      t.integer :last_post, :limit => 8
     end
   end
 
