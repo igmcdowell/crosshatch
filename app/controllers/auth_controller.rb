eadfd2e1b9e4f2bd43949a9f6fb9166ad45587e7
Ian McDowell <igmcdowell@gmail.com>
2011-06-01 19:19:32 -0700

    removing blocks to troubleshoot heroku error

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -18,51 +18,7 @@ class AuthController < ApplicationController
     redirect_to request_token.authorize_url
   end
   
-  def finishtwitter
-    oauth = OAuth::Consumer.new(ENV['TW_KEY'], ENV['TW_SECRET'],
-                                 { :site => "http://twitter.com" })
-    request_token = OAuth::RequestToken.new(oauth, session[:twtoken],
-                                            session[:twsecret])
-    
-    access_token = request_token.get_access_token(
-                      :auth_verifier => params[:oauth_verifier])
 
-
-    @token = access_token.token
-    token = @token
-    secret = access_token.secret
-          
-    # Get account details from Twitter
-    response = oauth.request(:get, '/account/verify_credentials.json', access_token, {  })
-    @data = JSON.parse(response.body)
-    twitter_id = @data['id']
-    twitter_img = @data['profile_image_url']
-    twitter_name = @data['screen_name']
-
-    if session[:twid]
-      #if we get here and we already have a session ID, then this must be the auth that's granting us permission, and we want to store the perms.
-      user = User.find_by_tw_id(session[:twid])
-      if token
-        user[:tw_linked] = true
-        user[:tw_token] = token
-        user[:tw_secret] = secret
-        user.save
-      end
-    else
-      #the user just logged in. We should check if they already exist in our database, and do appropriate updates.
-      user = User.find_by_tw_id(session[:twid])
-      unless user:
-        user = User.new(:tw_id => twitter_id, :tw_linked => false, :fb_linked => false)
-      end
-      #These attributes can change, so they get updated/created for all users on login
-      user[:tw_handle] = twitter_name
-      user[:tw_img] = twitter_img
-      user[:tw_name] = twitter_name
-      user.save
-      session[:twid] = twitter_id
-    end
-    render :success
-  end
   
   # GET /auth
   def validate
